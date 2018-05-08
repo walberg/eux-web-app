51abd560bf64acd9637ffbcbbca8d7a219cda95b
Ragnar Westad <ragnar.westad@nav.no>
2018-05-08 14:25:43 +0200

    Lagt til form med felter som er nærmere det vi skal ende opp med

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.scss';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      caseType: '',
+      user: '',
+      counterParty: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert('Case type: ' + this.state.caseType);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+      <form>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">EUX-web-app</h1>
+          </header>
+          <p className="App-intro">
+            Nei, altså ... prøver å få til en Ract-app da...
+          </p>
+        </div>
+        <div>
+          <label>Sakstype:</label>
+          <input type="text" value={this.state.value}/>
+        </div>
+        <div>
+          <label>User:</label>
+          <input type="text" value={this.state.user}/>
+        </div>
+        <div>
+          <label>Counterparty:</label>
+          <input type="text" value={this.state.counterParty}/>
+        </div>
+        <div>
+          <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+        </div>
+      </form>
     );
   }
 }
