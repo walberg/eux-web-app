6ab2e5c1f6d6098d6aa0ac7e00c816982802fcc4
abjolseth <abjolseth@gmail.com>
2018-06-22 21:05:19 +0200

    Flyttet Spesifikke selectors fra kodeverk til eusak

--- src/ducks/kodeverk/selectors.js
+++ src/ducks/kodeverk/selectors.js
@@ -22,60 +22,11 @@ export const sectorSelector = createSelector(
   sector => sector
 );
 
-export const valgtSectorSelector = createSelector(
-  state => (state.form.opprettSak ? state.form.opprettSak : {}),
-  opprettSakForm => {
-    const { values = {} } = opprettSakForm;
-    return values.sector;
-  }
-);
-export const valgtBucTypeSelector = createSelector(
-  state => (state.form.opprettSak ? state.form.opprettSak : {}),
-  opprettSakForm => {
-    const { values = {} } = opprettSakForm;
-    return values.buctype;
-  }
-);
-
-export const buctyperSelector = createSelector(
-  state => kodemapsSelector(state),
-  state => state.kodeverk.data.buctyper,
-  state => valgtSectorSelector(state),
-  (kodemaps, buctyper, valgtSector) => {
-    if (!kodemaps) { return []; }
-    if (!valgtSector) { return []; }
-    return buctyper[kodemaps.SECTOR2BUC[valgtSector]];
-  }
-);
-
 export const alleSEDtyperSelector = createSelector(
   state => state.kodeverk.data.sedtyper,
   sedtyper => sedtyper
 );
 
-export const sedtypeSelector = createSelector(
-  state => valgtSectorSelector(state),
-  state => kodemapsSelector(state),
-  state => alleSEDtyperSelector(state),
-  state => valgtBucTypeSelector(state),
-  (valgtSector, kodemaps, sedKodeverk, valgtBucType) => {
-    if (!(valgtSector && valgtSector === 'FB')) { return []; }
-    if (!kodemaps) { return []; }
-    if (!valgtBucType) { return []; }
-    const sedtyper = kodemaps.BUC2SEDS[valgtSector][valgtBucType];
-    if (!(sedtyper && sedtyper.length)) return [];
-    return sedtyper.reduce((acc, curr) => {
-      const kode = sedKodeverk.find(elem => elem.kode === curr);
-      acc.push(kode);
-      return acc;
-    }, []);
-  }
-);
-
-export const institusjonSelector = createSelector(
-  state => state.kodeverk.data.institusjoner,
-  institusjoner => institusjoner
-);
 export const familierelasjonerSelector = createSelector(
   state => state.kodeverk.data.familierelasjoner,
   familierelasjoner => familierelasjoner
