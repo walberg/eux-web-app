7d52a1c19eb65694f051f55c123c79fc7fcd681e
Bjølseth, Are <are.bjolseth@nav.no>
2018-06-27 23:12:33 +0200

    EUX-654 Utvid vedlegg med dokument søk.

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,7 @@ import * as Saksbehandler from './modules/saksbehandler';
 import * as Vedlegg from './modules/vedlegg';
 import * as Personer from './modules/personer';
 import * as Eusak from './modules/eusak';
+import * as Dokumenter from './modules/dokumenter';
 // from .env or .env.local
 // const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}`;
 // console.log('process.env', process.env);
@@ -11,6 +12,7 @@ import * as Eusak from './modules/eusak';
 
 export {
   Eusak,
+  Dokumenter,
   Kodeverk,
   Saksbehandler,
   Personer,
