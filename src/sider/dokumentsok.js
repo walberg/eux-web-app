9dcedd4387865e766fade34a1fae904999d761ed
Bjølseth, Are <are.bjolseth@nav.no>
2018-06-28 16:15:10 +0200

    La til 'opprettetdato' med moment formattering.

--- src/sider/dokumentsok.js
+++ src/sider/dokumentsok.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PT from 'prop-types';
+import moment from 'moment/moment';
 
 import * as Nav from '../utils/navFrontend';
 import * as Skjema from '../felles-komponenter/skjema';
 import * as API from '../services/api';
 import { DokumenterSelectors } from '../ducks/dokumenter';
 
+const yyyMMdd = dato => moment(dato).format('YYYY-MM-DD');
+
 const DokumentKort = ({ dokumenter }) => (
   <Nav.Panel className="dokumentsok__kort">
     <Skjema.Select feltNavn="rinadokumentID" label="Velg SED Type" bredde="xl">
-      {dokumenter && dokumenter.map(element => <option value={element.rinadokumentID} key={element.rinadokumentID}>{element.kode}-{element.rinadokumentID}</option>)}
+      {dokumenter && dokumenter.map(element => <option value={element.rinadokumentID} key={element.rinadokumentID}>{element.kode} =&gt; {yyyMMdd(element.opprettetdato)}</option>)}
     </Skjema.Select>
   </Nav.Panel>
 );
