f2fe01ed055896941ea36edacff218f3f3a9dd85
Bjølseth, Are <are.bjolseth@nav.no>
2018-07-16 10:54:04 +0200

    EUX-660 Endre endpunkt uri for Opprett Sak  /eusak => /rina/sak

--- src/services/modules/eusak.js
+++ src/services/modules/eusak.js
@@ -3,6 +3,6 @@ import { API_BASE_URL } from '../api-constants';
 
 // eslint-disable-next-line import/prefer-default-export
 export function send(data) {
-  const URI_OPPRETT_EUSAK = `${API_BASE_URL}eusak`;
+  const URI_OPPRETT_EUSAK = `${API_BASE_URL}rina/sak`;
   return postAsJson(URI_OPPRETT_EUSAK, data);
 }
