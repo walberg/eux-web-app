b77e2c9974b180dba2bf8078f19371872c862dc7
Are Bjolseth <abjolseth@gmail.com>
2019-01-12 03:22:00 +0100

    Eslint fixews

--- src/felles-komponenter/skjema/PersonOgFamilieRelasjoner/FamilieRelasjonsComponent.js
+++ src/felles-komponenter/skjema/PersonOgFamilieRelasjoner/FamilieRelasjonsComponent.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import { FieldArray } from 'redux-form';
-import PT from 'prop-types';
 
-import * as MPT from '../../../proptypes/';
 import * as Nav from '../../../utils/navFrontend';
 import FamilieRelasjonController from './FamilieRelasjonController';
 
-const FamilieRelasjonsComponent = ({ relasjoner }) => (
+const FamilieRelasjonsComponent = () => (
   <div>
     <Nav.Systemtittel>Familierelasjon(er)</Nav.Systemtittel>
     <Nav.Panel border>
       <Nav.Fieldset legend="Vennligst velg familirelasjonen SEDen angår:" className="familieRelasjoner">
         <div className="familieRelasjoner__liste">
-          {relasjoner.length === 0 && <Nav.Panel className="familieRelasjoner__liste__tom">Valgte familierelasjoner(0)</Nav.Panel>}
           <FieldArray name="tilleggsopplysninger.familierelasjoner" component={FamilieRelasjonController} />
         </div>
       </Nav.Fieldset>
     </Nav.Panel>
   </div>
 );
-FamilieRelasjonsComponent.propTypes = {
-  relasjoner: PT.arrayOf(MPT.FamilieRelasjon),
-};
-FamilieRelasjonsComponent.defaultProps = {
-  relasjoner: [],
-};
 
 export default FamilieRelasjonsComponent;
