3909b8280fa5fb85b4a225028ec0e44e0c3d5e02
Even Haasted <even.haasted@nav.no>
2019-10-02 14:55:43 +0200

    ArbeidsforholdLinje tester

--- src/sider/sak/arbeidsforhold.js
+++ src/sider/sak/arbeidsforhold.js
@@ -3,43 +3,9 @@ import { FieldArray } from 'redux-form';
 import PT from 'prop-types';
 
 import * as MPT from '../../proptypes';
-import * as Ikoner from '../../resources/images/index';
-import * as Nav from '../../utils/navFrontend';
-import { formatterDatoTilNorsk } from '../../utils/dato';
+import ArbeidsforholdLinje from './ArbeidsforholdLinje';
 
 const uuid = require('uuid/v4');
-
-const btnStyle = {
-  margin: '1.25em 0 0 0',
-};
-
-const ArbeidsforholdLinje = props => {
-  const { arbeidsforholdet, erValgt, arbeidsforholdKlikkHandler } = props;
-  const {
-    arbeidsforholdIDnav, navn, orgnr, ansettelsesPeriode: { fom, tom },
-  } = arbeidsforholdet;
-  return (
-    <Nav.Row>
-      <Nav.Column xs="1" style={{ width: '5%' }}>
-        <div className="panelheader__ikon" style={{ marginTop: '0.25em', backgroundImage: `url('${Ikoner.Arbeidsforhold}')` }} />
-      </Nav.Column>
-      <Nav.Column xs="2" style={{ width: '20%', marginBottom: '0.5em' }}>
-        <strong>{navn}</strong><br />Orgnr:&nbsp;{orgnr}<br />StartDato:&nbsp;{formatterDatoTilNorsk(fom)}<br />SluttDato:&nbsp;{formatterDatoTilNorsk(tom)}
-      </Nav.Column>
-      <Nav.Column xs="1" style={btnStyle} >
-        <Nav.Checkbox checked={erValgt} onChange={() => arbeidsforholdKlikkHandler(arbeidsforholdIDnav)} label="Velg" />
-      </Nav.Column>
-    </Nav.Row>
-  );
-};
-ArbeidsforholdLinje.propTypes = {
-  arbeidsforholdet: MPT.Arbeidsforholdet.isRequired,
-  erValgt: PT.bool,
-  arbeidsforholdKlikkHandler: PT.func.isRequired,
-};
-ArbeidsforholdLinje.defaultProps = {
-  erValgt: false,
-};
 
 class ArbeidsforholdListe extends Component {
   arbeidsforholdKlikkHandler = arbeidsforholdIDnav => {
