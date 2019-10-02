9f44fcafd76967db63ef652b653b03838ba52c5d
Even Haasted <even.haasted@nav.no>
2019-10-02 15:23:51 +0200

    Tester for arbeidsforhold.js

--- src/sider/sak/arbeidsforhold.js
+++ src/sider/sak/arbeidsforhold.js
@@ -1,50 +1,8 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import { FieldArray } from 'redux-form';
-import PT from 'prop-types';
 
-import * as MPT from '../../proptypes';
-import ArbeidsforholdLinje from './ArbeidsforholdLinje';
+import ArbeidsforholdListe from './ArbeidsforholdListe';
 
-const uuid = require('uuid/v4');
-
-class ArbeidsforholdListe extends Component {
-  arbeidsforholdKlikkHandler = arbeidsforholdIDnav => {
-    const { fields, arbeidsforhold } = this.props;
-    const alleValgteFelter = fields.getAll() || [];
-    const valgtArbeidsgiver = arbeidsforhold.find(elem => elem.arbeidsforholdIDnav === arbeidsforholdIDnav);
-    const eksistererVedPosisjon = alleValgteFelter.findIndex(item => item.arbeidsforholdIDnav === arbeidsforholdIDnav);
-    if (eksistererVedPosisjon === -1) {
-      fields.push({ ...valgtArbeidsgiver });
-    } else {
-      fields.remove(eksistererVedPosisjon);
-    }
-  };
-  render() {
-    const { fields, arbeidsforhold } = this.props;
-    const alleValgteFelter = fields.getAll();
-    return (
-      <Fragment>
-        {arbeidsforhold.map(arbeidsforholdet => {
-          const arbeidsForholdErValgt = alleValgteFelter.find(item => item.arbeidsforholdIDnav === arbeidsforholdet.arbeidsforholdIDnav);
-          return <ArbeidsforholdLinje
-            key={uuid()}
-            arbeidsforholdet={arbeidsforholdet}
-            erValgt={arbeidsForholdErValgt !== undefined}
-            arbeidsforholdKlikkHandler={this.arbeidsforholdKlikkHandler}
-          />;
-        })}
-      </Fragment>
-    );
-  }
-}
-
-ArbeidsforholdListe.propTypes = {
-  fields: PT.object.isRequired,
-  arbeidsforhold: MPT.Arbeidsforhold,
-};
-ArbeidsforholdListe.defaultProps = {
-  arbeidsforhold: [],
-};
 
 const Arbeidsforhold = props => (
   <div className="arbeidsforhold">
