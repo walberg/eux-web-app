fa4991689d33844656b6b96f4786051f7d112d68
Are Bjolseth <abjolseth@gmail.com>
2019-10-04 22:16:31 +0200

    Fine tuning and updated module version in package.json

--- src/komponenter/DokumentKort.js
+++ src/komponenter/DokumentKort.js
@@ -6,12 +6,11 @@ import * as Skjema from '../felles-komponenter/skjema';
 import * as Utils from '../utils';
 
 import './dokumentsok.css';
-// =&gt; {Utils.dato.formatterDatoTilNorsk(element.opprettetdato)}
 
-const DokumentKort = ({dokumenter}) => {
-  const optionValue = element => {
+const DokumentKort = ({ dokumenter }) => {
+  const optionText = element => {
     let opprettetDatoString = '';
-    if (element && element.opprettetdato) {
+    if (element.opprettetdato) {
       const dato = Utils.dato.formatterDatoTilNorsk(element.opprettetdato);
       opprettetDatoString = ` => ${dato}`;
     }
@@ -20,14 +19,15 @@ const DokumentKort = ({dokumenter}) => {
   return (
     <Nav.Panel className="dokumentsok__kort">
       <Skjema.Select id="id-rinadokument" feltNavn="rinadokumentID" label="Velg SED Type" bredde="xl">
-        {dokumenter && dokumenter.map(element =>
-          <option
-            value={element.rinadokumentID}
-            key={element.rinadokumentID}>{optionValue(element)}
-          </option>)}
+        {
+          dokumenter && dokumenter.map(element =>
+            <option value={element.rinadokumentID} key={element.rinadokumentID}>
+              {optionText(element)}
+            </option>)
+        }
       </Skjema.Select>
     </Nav.Panel>
-  )
+  );
 };
 DokumentKort.propTypes = {
   dokumenter: PT.array,
