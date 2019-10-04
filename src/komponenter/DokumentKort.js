0e2825907b9b296c18327cb287c5f5e9fa2acdb4
Are Bjolseth <abjolseth@gmail.com>
2019-10-04 21:47:23 +0200

    Vedlegg dokument. Bugfix i "Velg SED Type" nedtrekksliste.

--- src/komponenter/DokumentKort.js
+++ src/komponenter/DokumentKort.js
@@ -6,19 +6,29 @@ import * as Skjema from '../felles-komponenter/skjema';
 import * as Utils from '../utils';
 
 import './dokumentsok.css';
+// =&gt; {Utils.dato.formatterDatoTilNorsk(element.opprettetdato)}
 
-
-const DokumentKort = ({ dokumenter }) => (
-  <Nav.Panel className="dokumentsok__kort">
-    <Skjema.Select id="id-rinadokument" feltNavn="rinadokumentID" label="Velg SED Type" bredde="xl">
-      {dokumenter && dokumenter.map(element =>
-        <option
-          value={element.rinadokumentID}
-          key={element.rinadokumentID}>{element.kode} =&gt; {Utils.dato.formatterDatoTilNorsk(element.opprettetdato)}
-        </option>)}
-    </Skjema.Select>
-  </Nav.Panel>
-);
+const DokumentKort = ({dokumenter}) => {
+  const optionValue = element => {
+    let opprettetDatoString = '';
+    if (element && element.opprettetdato) {
+      const dato = Utils.dato.formatterDatoTilNorsk(element.opprettetdato);
+      opprettetDatoString = ` => ${dato}`;
+    }
+    return `${element.kode}${opprettetDatoString}`;
+  };
+  return (
+    <Nav.Panel className="dokumentsok__kort">
+      <Skjema.Select id="id-rinadokument" feltNavn="rinadokumentID" label="Velg SED Type" bredde="xl">
+        {dokumenter && dokumenter.map(element =>
+          <option
+            value={element.rinadokumentID}
+            key={element.rinadokumentID}>{optionValue(element)}
+          </option>)}
+      </Skjema.Select>
+    </Nav.Panel>
+  )
+};
 DokumentKort.propTypes = {
   dokumenter: PT.array,
 };
