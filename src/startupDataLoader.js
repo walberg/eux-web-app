6873971ac2d5e620748e9048d4a980031b7751bb
Are Bjølseth <are.bjolseth@nav.no>
2019-11-29 13:46:10 +0100

    Reapplied frontendlogger

--- src/startupDataLoader.js
+++ src/startupDataLoader.js
@@ -1,9 +1,24 @@
+import * as Utils from './utils';
 import { KodeverkOperations } from './ducks/kodeverk/';
-import { saksbehandlerOperations } from './ducks/saksbehandler/';
+import { saksbehandlerOperations, saksbehandlerTypes } from './ducks/saksbehandler/';
 import { ServerinfoOperations } from './ducks/serverinfo/';
 
-export default function loadInitialData(store) {
-  store.dispatch(KodeverkOperations.preload());
-  store.dispatch(saksbehandlerOperations.hent());
-  store.dispatch(ServerinfoOperations.hent());
+export default async function loadInitialData(store) {
+  let res;
+  try {
+    window.frontendlogger.info(Utils.buildinfo());
+    await store.dispatch(KodeverkOperations.preload());
+    await store.dispatch(ServerinfoOperations.hent());
+    res = await store.dispatch(saksbehandlerOperations.hent());
+    if (res && res.type === saksbehandlerTypes.OK) {
+      window.frontendlogger.info(res.data.brukernavn);
+    }
+  } catch (e) {
+    console.log(e); // eslint-disable-line no-console
+    window.frontendlogger.error({
+      e,
+      stack: e.stack,
+      res,
+    });
+  }
 }
